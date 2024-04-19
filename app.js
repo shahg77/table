743a859f7bb09c371cef1859d36b7892190e34ff
shahg77 <167567176+shahg77@users.noreply.github.com>
2024-04-19 11:15:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+document.getElementById("btn").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.getElementById("output").innerHTML = "";
+  let tableNo = parseInt(document.getElementById("input").value);
+  if (tableNo < 0) {
+    tableNo = Math.abs(tableNo);
+  }
+  for (let i = tableNo; i <= tableNo * 1; i = i + tableNo) {
+    for (let j = 1; j <= 10; j++) {
+      document.getElementById("output").innerHTML += `${i} x ${j} = ${
+        i * j
+      }<br>`;
+    }
+  }
+});
